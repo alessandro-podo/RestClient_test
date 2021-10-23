d27ee577c68cc2b54c5e8e9a65bf2806ce1595e5
Alessandro Podo <alessandro.podo@lrz.de>
2021-10-24 00:15:16 +0200

    ReturnType angepasst

--- src/Resources/skeleton/makeSuccessHandler.tpl.php
+++ src/Resources/skeleton/makeSuccessHandler.tpl.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace <?php echo $namespace; ?>;
 
 use RestClient\Interfaces\RestClientResponseInterface;
+use RestClient\DefaultHandler\SuccessHandler;
 
-class <?php echo $class_name; ?> extends RestClientResponseInterface
+class <?php echo $class_name; ?> extends SuccessHandler
 {
-public function getResult(): <?php echo $dtoName; ?>
-{
-try {
-return $this->serializer->denormalize($this->response->toArray(), <?php echo $dtoName; ?>::class)->setStatusCode($this->response->getStatusCode());
-} catch (\Throwable $throwable) {
+    /**
+    * @return <?php echo $dtoName; ?>|RestClientResponseInterface
+    */
+    public function getResult(): RestClientResponseInterface
+    {
+        try {
+            return $this->serializer->denormalize($this->response->toArray(), <?php echo $dtoName; ?>::class)->setStatusCode($this->response->getStatusCode());
+        } catch (\Throwable $throwable) {
 
-}
-}
+        }
+    }
 }
